48a94a052cd7c25c485e2911812febf8c17f787b
Nsarkar-xlr8 <knayemlol24@gmail.com>
2026-04-07 21:34:38 +0600

    refactor: replace QueueModule with CommonModule in ContactModule imports

--- src/contact/contact.module.ts
+++ src/contact/contact.module.ts
@@ -3,16 +3,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ContactInquiry, ContactInquirySchema } from '../database/schemas/contact-inquiry.schema';
 import { ContactService } from './contact.service';
 import { ContactController } from './contact.controller';
-import { QueueModule } from '../common/modules/queue.module';
+import { CommonModule } from '../common/common.module';
 import { AuthUser, AuthUserSchema } from '../database/schemas/auth-user.schema';
 
 @Module({
   imports: [
+    CommonModule,
     MongooseModule.forFeature([
       { name: ContactInquiry.name, schema: ContactInquirySchema },
-      { name: AuthUser.name, schema: AuthUserSchema }, // Required for AuthGuard
     ]),
-    QueueModule,
   ],
   controllers: [ContactController],
   providers: [ContactService],
