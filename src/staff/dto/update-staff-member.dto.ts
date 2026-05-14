172f543b32b861ec9040c8531cffb5e290e1a41c
jarifMohammed <mohammedjarif@protonmail.com>
2026-05-14 16:54:08 +0600

    fix: update UpdateStaffMemberDto to clarify businessId handling and improve validation

--- src/staff/dto/update-staff-member.dto.ts
+++ src/staff/dto/update-staff-member.dto.ts
@@ -1,13 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateStaffMemberDto } from './create-staff-member.dto';
 import { OmitType } from '@nestjs/mapped-types';
-import { IsBoolean, IsOptional } from 'class-validator';
-import { Exclude } from 'class-transformer';
+import { IsBoolean, IsOptional, ValidateIf } from 'class-validator';
 
 export class UpdateStaffMemberDto extends PartialType(
   OmitType(CreateStaffMemberDto, ['businessId'] as const),
 ) {
-  @Exclude()
+  // businessId will be ignored if sent by client - it's read-only
+  @ValidateIf(() => false)
   businessId?: string;
 
   @IsBoolean()
