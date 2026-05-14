e167bf2021db9aa0432462c183ba09c5b73878dd
jarifMohammed <mohammedjarif@protonmail.com>
2026-05-14 16:47:37 +0600

    feat: add businessId exclusion to UpdateStaffMemberDto for improved data handling

--- src/staff/dto/update-staff-member.dto.ts
+++ src/staff/dto/update-staff-member.dto.ts
@@ -2,10 +2,14 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateStaffMemberDto } from './create-staff-member.dto';
 import { OmitType } from '@nestjs/mapped-types';
 import { IsBoolean, IsOptional } from 'class-validator';
+import { Exclude } from 'class-transformer';
 
 export class UpdateStaffMemberDto extends PartialType(
   OmitType(CreateStaffMemberDto, ['businessId'] as const),
 ) {
+  @Exclude()
+  businessId?: string;
+
   @IsBoolean()
   @IsOptional()
   isActive?: boolean;
